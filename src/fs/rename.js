faa8411f451a9500b6ad3bd91a5ce424319dad36
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-04-26 13:26:29 +0300

    feat: half fn of fs done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { rename as rn } from "fs/promises";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const basePath = path.join(import.meta.dirname, "files");
+
+  rn(
+    path.join(basePath, "wrongFilename.txt"),
+    path.join(basePath, "properFilename.md")
+  ).catch(() => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await rename();
