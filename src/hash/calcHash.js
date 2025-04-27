ac4a69069eb70b54363dbd0d70da32b498cea197
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-04-27 20:02:02 +0300

    feat: src/hash is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import crypto from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+
+  const stream = createReadStream(
+    path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
