e265f6a8c8e0a83cb6e33d4b6fd7da5c0f41b1e2
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-04-28 15:56:07 +0300

    feat: src/cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { spawn } from "node:child_process";
+import path from "node:path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  if (!args) return;
+
+  const filePath = path.join(import.meta.dirname, "files", "script.js");
+  const sp = spawn("node", [filePath, ...args]);
+
+  sp.stdout.on("data", (chunk) => process.stdout.write(chunk));
+  process.stdin.on("data", (data) => sp.stdin.write(data));
+
+  sp.on("close", () => process.exit());
 };
 
 // Put your arguments in function call to test this functionality
