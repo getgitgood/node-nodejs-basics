efe8377cce5c41dc7827a136ac44611f6436c88a
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-04-28 22:01:13 +0300

    refactor: simplify

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -5,11 +5,9 @@ const spawnChildProcess = async (args) => {
   if (!args) return;
 
   const filePath = path.join(import.meta.dirname, "files", "script.js");
-  const sp = fork(filePath, args, { stdio: ["pipe", "pipe", "ipc"] });
 
-  process.stdin.pipe(sp.stdin);
-  sp.stdout.pipe(process.stdout);
+  fork(filePath, args, { stdio: ["inherit", "inherit", "ipc"] });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['test1', 'test2']);
